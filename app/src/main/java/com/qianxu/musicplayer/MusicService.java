aeaeb3cd2b18a5c1b0c4777783f7e44d35576c3d
qxdn <1464238196@qq.com>
2019-12-17 14:07:36 +0800

    自动播放下一首

--- app/src/main/java/com/qianxu/musicplayer/MusicService.java
+++ app/src/main/java/com/qianxu/musicplayer/MusicService.java
@@ -7,12 +7,17 @@ import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 
 public class MusicService extends Service {
+
+    private LocalBroadcastManager localBroadcastManager;
 
     private MediaPlayer mediaPlayer;
 
     public MusicService() {
+        localBroadcastManager=LocalBroadcastManager.getInstance(this);
     }
 
     @Override
@@ -32,6 +37,15 @@ public class MusicService extends Service {
             mediaPlayer.reset();
         try {
             mediaPlayer=MediaPlayer.create(this, Uri.parse(path));
+            //播放完成监听
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    Intent intent=new Intent("com.qianxu.musicplayer.LOCAL_BROADCAST_MCOMPLETE");
+                    intent.putExtra("finishcode",1);
+                    localBroadcastManager.sendBroadcast(intent);
+                }
+            });
             mediaPlayer.start();
         }catch (Exception e){
             e.printStackTrace();
